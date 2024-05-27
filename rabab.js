e1793ab72c7f454a9003c90d65a8b380da810592
Rabab-Soomro <nalishba76@gmail.com>
2024-05-27 10:30:54 +0500

    Add files via upload

---
+++ rabab.js
@@ -0,0 +1,4 @@
+//Q 5:Famous quote 2:Repeat exercise 4, but this time store the famous person's name in a variable called Famous-person.Then compose your message and store it in a new variable called message.print your message.
+var famousperson = "Albert Einstein";
+var message = "once said,'A person who never made a mistaake never tried anything new.'";
+console.log(famousperson, message);
